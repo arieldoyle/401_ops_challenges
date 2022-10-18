65dde2e7c3dd0ace8df3efdec4f21e2e01cf6ae7
Ariel Doyle (they/them) <72620942+arieldoyle@users.noreply.github.com>
2022-10-18 18:02:59 -0500

    Update tcpscanner.py

--- tcpscanner.py
+++ tcpscanner.py
@@ -21,7 +21,6 @@
         # Command: sudo pip install --pre scapy[complete]
     # Script has to be run as sudo
 
-
 # Import necessary libraries for called functions
 from scapy.all import ICMP, IP, sr1, sr, TCP
 import random
@@ -31,20 +30,29 @@ import random
 host = "scanme.nmap.org" 
 
 # Define variable port_range providing a port range
-port_range = [21, 22, 23] 
+port_range = [22, 23, 80, 443, 3389] 
 
 # Main
+# Create variable host to ask for user input on ip address to scan
+host = input("Enter an IP Address to scan: ")
+
+# Create port_range variable with ports to scan
+port_range = [22, 23, 80, 443, 3389]
+
 # Send SYN with random source ports for each destination port
 for dst_port in port_range:
+    # Randomizes the source port (src_port variable)
     src_port = random.randint(1025,65534)
-    port_string = "".join(map(chr, bytes(dst_port)))
+    # Turns the destination port (dst_port variable) into a string
+    port_string = str(dst_port)
+    # Creates response (rspns variable) to hold port statuses
     rspns = sr1(IP(dst=host)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=1,verbose=0)
+    # Port Filtered and silently dropped
     if rspns is None:
-        # Port Filtered and silently dropped
         print ("Port " + port_string + ": Packet was filtered and dropped")
         print (rspns)
+    # Port open for 0x12
     elif rspns.haslayer(TCP):
-        # Port open for 0x12
         if rspns.getlayer(TCP).flags == 0x12: 
             print("Port " + port_string + ": Open")
             # Send RST packet to graciously close open connection
